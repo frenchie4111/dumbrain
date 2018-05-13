5b25cfa21892048dc8e9eab38d7d97f4ce671f7f
Michael Lyons <mdl0394@gmail.com>
2018-05-13 13:40:52 -0700

    Added sorting

--- rl/retro-contest/feature-map-data-generator/TemplateMatcher.py
+++ rl/retro-contest/feature-map-data-generator/TemplateMatcher.py
@@ -6,14 +6,20 @@ class TemplateMatcher():
     def __init__( self, templates ):
         self.templates = templates
         self._processed_templates = []
+        self._frequencies = []
         self.init()
 
     def init( self ):
         for template in self.templates:
             self._processed_templates.append( self.preprocessTemplate( template ) )
+        self._processed_templates = np.array( self._processed_templates )
+        self._frequencies = np.zeros( len( self._processed_templates ) )
 
+    def updateFrequencies( self, winner ):
+        self._frequencies[ winner ] += 1
+            
     def getTemplates( self ):
-        return self._processed_templates
+        return self._processed_templates[ np.argsort( self._frequencies ) ]
 
     def preprocessFrame( self, frame ):
         return frame
@@ -24,7 +30,9 @@ class TemplateMatcher():
     def getMask( self, template, template_i ):
         return None
     
-    def shortCircuit( self, score ):
+    def shortCircuit( self, results ):
+        if results[ 0 ] > 0.95:
+            return True
         return False
     
     def matchTemplate( self, frame ):
@@ -32,7 +40,7 @@ class TemplateMatcher():
 
         all_results = []
 
-        for template in self.getTemplates():
+        for template_i, template in enumerate( self.getTemplates() ):
             template, mask = template
 
             w, h = template.shape[ 0:2 ]
@@ -45,10 +53,12 @@ class TemplateMatcher():
             results = score, ( max_loc[ 0 ], max_loc[ 1 ] ), ( max_loc[ 0 ] + h, max_loc[ 1 ] + w  ), time.clock() - start_time
 
             if( self.shortCircuit( results ) ):
+                self.updateFrequencies( template_i )
                 return results
             
             all_results.append( results )
         
         scores = [ sprite_results[ 0 ] for sprite_results in all_results ]
         highest_score_idx = np.argmax( scores )
+        self.updateFrequencies( highest_score_idx )
         return all_results[ highest_score_idx ]
