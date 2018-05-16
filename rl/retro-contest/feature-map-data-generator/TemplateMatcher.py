950ab4151c4e7281bbbfe335fa5589f948b8ab70
Michael Lyons <mdl0394@gmail.com>
2018-05-15 19:02:02 -0700

    Added previous position to TemplateMatcher.py

--- rl/retro-contest/feature-map-data-generator/TemplateMatcher.py
+++ rl/retro-contest/feature-map-data-generator/TemplateMatcher.py
@@ -20,7 +20,8 @@ class TemplateMatcher():
         self._frequencies[ winner ] += 1
             
     def getTemplates( self ):
-        return self._processed_templates[ np.argsort( self._frequencies ) ]
+        argsorted = np.argsort( self._frequencies )
+        return self._processed_templates[ argsorted ], argsorted
 
     def preprocessFrame( self, frame ):
         return frame
@@ -41,37 +42,48 @@ class TemplateMatcher():
 
         all_results = []
 
-        search_frames = [ frame ]
+        search_frames = [ ( frame, ( 0, 0 ) ) ]
 
         frame_width = 5
         if previous is not None:
             previous_tl, previous_br = previous
             previous_frame = frame[ previous_tl[ 1 ] - frame_width : previous_br[ 1 ] + frame_width, previous_tl[ 0 ] - frame_width : previous_br[ 0 ] + frame_width ]
-            search_frames.insert( 0, previous_frame )
+            print( previous_tl )
+            search_frames.insert( 0, ( previous_frame, ( previous_tl[ 0 ] - frame_width, previous_tl[ 1 ] - frame_width ) ) )
 
-        for search_frame in search_frames:
-            for template_i, template in enumerate( self.getTemplates() ):
+        for search_frame_info in search_frames:
+            search_frame, offset = search_frame_info
+
+            templates, template_idx_map = self.getTemplates()
+            for template_i, template in enumerate( templates ):
                 template, mask = template
                 
                 if search_frame.shape[ 0 ] < template.shape[ 0 ] or search_frame.shape[ 1 ] < template.shape[ 1 ]:
                     continue
 
-                w, h = template.shape[ 0:2 ]
+                w, h = template.shape[ 0 : 2 ]
 
                 start_time = time.clock()
 
                 results = cv.matchTemplate( search_frame, template, cv.TM_CCORR_NORMED, mask=mask )
                 _, score, _, max_loc = cv.minMaxLoc( results )
+                
+                x1 = max_loc[ 0 ] + offset[ 0 ]
+                y1 = max_loc[ 1 ] + offset[ 1 ]
 
-                results = score, ( max_loc[ 0 ], max_loc[ 1 ] ), ( max_loc[ 0 ] + h, max_loc[ 1 ] + w  ), time.clock() - start_time
+                x2 = x1 + h
+                y2 = x1 + w
+
+                results = score, ( x1, y1 ), ( x2, y2 ), template_idx_map[ template_i ]
 
                 if( self.shortCircuit( results ) ):
-                    self.updateFrequencies( template_i )
+                    print( max_loc[ 0 ], max_loc[ 1 ], offset[ 1 ], offset[ 0 ], x1, y1, x2, y2 )
+                    self.updateFrequencies( template_i % len( frame ) )
                     return results
 
                 all_results.append( results )
 
         scores = [ sprite_results[ 0 ] for sprite_results in all_results ]
         highest_score_idx = np.argmax( scores )
-        self.updateFrequencies( highest_score_idx )
+        self.updateFrequencies( highest_score_idx % len( frame ) )
         return all_results[ highest_score_idx ]
