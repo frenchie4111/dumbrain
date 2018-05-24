64c40fdef730d257206be9199cc19969610ab87f
Michael Lyons <mdl0394@gmail.com>
2018-05-24 10:08:29 -0700

    Changed download to accept a loading_bar class

--- dumbrain/lib/download.py
+++ dumbrain/lib/download.py
@@ -6,7 +6,7 @@ import time
 import tempfile
 from zipfile import ZipFile
 
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 import requests
 import urllib
 
@@ -18,7 +18,7 @@ def mkdirp( folder ):
         print( f'Directory {folder} didn\'t exist, making' )
         os.makedirs( folder )
 
-def download( url, dest_file, show_progress=True ):
+def download( url, dest_file, show_progress=True, loading_bar=tqdm ):
     """
     Downloads file to given file. Path to file should exist. Uses _download_pbar
     global
@@ -27,35 +27,35 @@ def download( url, dest_file, show_progress=True ):
     def reporthook( count, part_size, total_size ):
         global _download_pbar
         if _download_pbar is None:
-            _download_pbar = tqdm( total = total_size )
+            _download_pbar = loading_bar( total = total_size )
         _download_pbar.update( part_size )
-        
+
     urllib.request.urlretrieve( url, dest_file, reporthook=reporthook )
     global _download_pbar
     _download_pbar.close()
     _download_pbar = None
 
-def unzip( zip_filename, dest_folder ):
+def unzip( zip_filename, dest_folder, loading_bar=tqdm ):
     zip_file = ZipFile( zip_filename )
 
     all_files = zip_file.infolist()
     uncompressed_size = sum( ( file.file_size for file in zip_file.infolist() ) )
 
     files = zip_file
-    with tqdm( total=uncompressed_size ) as pbar:
+    with loading_bar( total=uncompressed_size ) as pbar:
         for file in all_files:
             zip_file.extract( file, dest_folder )
             pbar.update( file.file_size )
 
-def downloadAndUnzip( download_url, save_folder ):
+def downloadAndUnzip( download_url, save_folder, loading_bar=tqdm ):
     mkdirp( save_folder )
 
     temp_zip_filename = 'temp.zip'
 
     with tempfile.TemporaryDirectory() as temp_dir:
         full_temp_zip_filename = path.join( temp_dir, temp_zip_filename )
-        download( download_url, full_temp_zip_filename )
-        unzip( full_temp_zip_filename, save_folder )
+        download( download_url, full_temp_zip_filename, loading_bar=loading_bar )
+        unzip( full_temp_zip_filename, save_folder, loading_bar=loading_bar )
 
 def validateArgs( argv ):
     if not len( argv ) == 3:
