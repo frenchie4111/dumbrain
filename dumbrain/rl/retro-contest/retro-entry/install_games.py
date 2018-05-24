82d0c7643bcc4ddd5be673c3a4fa68773177c12b
Michael Lyons <mdl0394@gmail.com>
2018-05-24 10:08:52 -0700

    Fixed up install_games

--- dumbrain/rl/retro-contest/retro-entry/install_games.py
+++ dumbrain/rl/retro-contest/retro-entry/install_games.py
@@ -1,8 +1,13 @@
-import lib
+from dumbrain.lib.download import downloadAndUnzip
 import retro.data
+import os
+from tqdm import tqdm
 
-romdir = 'data/roms/'
+def main():
+    romdir = 'data/roms/'
+    downloadAndUnzip( 'http://aiml.mikelyons.org/datasets/sonic/Sonic%20Roms.zip', romdir, loading_bar=tqdm )
+    roms = [ os.path.join( romdir, rom ) for rom in os.listdir( romdir ) ]
+    retro.data.merge( *roms, quiet=False )
 
-download.downloadAndUnzip( 'http://aiml.mikelyons.org/datasets/sonic/Sonic%20Roms.zip', romdir )
-roms = [ os.path.join( romdir, rom ) for rom in os.listdir( romdir ) ]
-retro.data.merge(*roms, quiet=False)
+if __name__ == '__main__':
+    main()
