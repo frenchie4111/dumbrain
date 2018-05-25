22d229db68e38a1f52887116095f827833b788e8
Michael Lyons <mdl0394@gmail.com>
2018-05-25 12:12:05 -0700

    Added colabInstallGames function

--- dumbrain/rl/retro_contest/install_games.py
+++ dumbrain/rl/retro_contest/install_games.py
@@ -13,12 +13,15 @@ def installGamesFromDir( romdir ):
     roms = [ os.path.join( romdir, rom ) for rom in os.listdir( romdir ) ]
     retro.data.merge( *roms, quiet=False )
 
+def colabInstallGames( romdir='data/roms/' ):
+    uploadColabAndUnzip( romdir )
+    installGamesFromDir( romdir )
+
 def main():
     parser = argparse.ArgumentParser( description='Installs retro games' )
 
     parser.add_argument( 'download_url', type=str, default=None, nargs='?', help='Download url for zip file' )
     parser.add_argument( '--romdir', type=str, default='data/roms/', help='Location to store the unzip roms' )
-    parser.add_argument( '--colab-upload', action='store_true', help='Enables colab file upload' )
 
     args = parser.parse_args()
 
@@ -26,8 +29,6 @@ def main():
 
     if args.download_url:
         downloadAndUnzip( args.download_url, args.romdir )
-    elif args.colab_upload:
-        uploadColabAndUnzip( args.romdir )
     else:
         raise RuntimeError( 'No .zip file method specified' )
 
